879e9b1005587a875af725840e99a4a1fa45dbc2
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2024-10-31 13:58:42 +0100

    Base game is done.

--- TypeReactionApp/Form1.Designer.cs
+++ TypeReactionApp/Form1.Designer.cs
@@ -29,11 +29,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_randomLetter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox_reaction = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_reactionTimes = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_average = new System.Windows.Forms.Label();
+            this.label_std = new System.Windows.Forms.Label();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_randomLetter
+            // 
+            this.label_randomLetter.Font = new System.Drawing.Font("Open Sans", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_randomLetter.Location = new System.Drawing.Point(12, 46);
+            this.label_randomLetter.Name = "label_randomLetter";
+            this.label_randomLetter.Size = new System.Drawing.Size(169, 199);
+            this.label_randomLetter.TabIndex = 4;
+            this.label_randomLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Letter:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtBox_reaction
+            // 
+            this.txtBox_reaction.Location = new System.Drawing.Point(12, 263);
+            this.txtBox_reaction.Name = "txtBox_reaction";
+            this.txtBox_reaction.Size = new System.Drawing.Size(169, 22);
+            this.txtBox_reaction.TabIndex = 0;
+            this.txtBox_reaction.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBox_reaction_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Typebox:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // listBox_reactionTimes
+            // 
+            this.listBox_reactionTimes.FormattingEnabled = true;
+            this.listBox_reactionTimes.ItemHeight = 15;
+            this.listBox_reactionTimes.Location = new System.Drawing.Point(201, 46);
+            this.listBox_reactionTimes.Name = "listBox_reactionTimes";
+            this.listBox_reactionTimes.Size = new System.Drawing.Size(169, 199);
+            this.listBox_reactionTimes.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(201, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Reaction average: ";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(201, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Standard deviation:";
+            // 
+            // label_average
+            // 
+            this.label_average.Location = new System.Drawing.Point(311, 245);
+            this.label_average.Name = "label_average";
+            this.label_average.Size = new System.Drawing.Size(59, 20);
+            this.label_average.TabIndex = 4;
+            this.label_average.Text = "0 ms";
+            this.label_average.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label_std
+            // 
+            this.label_std.Location = new System.Drawing.Point(311, 265);
+            this.label_std.Name = "label_std";
+            this.label_std.Size = new System.Drawing.Size(59, 20);
+            this.label_std.TabIndex = 4;
+            this.label_std.Text = "0 ms";
+            this.label_std.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(201, 296);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(169, 34);
+            this.btn_Exit.TabIndex = 2;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 296);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(169, 34);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Open Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(0, 1, 1, 1);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.fileToolStripMenuItem });
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(382, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.saveToFileToolStripMenuItem, this.saveAsToolStripMenuItem, this.loadFromFileToolStripMenuItem, this.loadFromToolStripMenuItem });
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            this.saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            this.saveToFileToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveToFileToolStripMenuItem.Text = "Save";
+            this.saveToFileToolStripMenuItem.ToolTipText = "CTRL + S";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.ToolTipText = "CTRL + ALT + S";
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            this.loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            this.loadFromFileToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.loadFromFileToolStripMenuItem.Text = "Load";
+            this.loadFromFileToolStripMenuItem.ToolTipText = "CTRL + O";
+            // 
+            // loadFromToolStripMenuItem
+            // 
+            this.loadFromToolStripMenuItem.Name = "loadFromToolStripMenuItem";
+            this.loadFromToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.loadFromToolStripMenuItem.Text = "Load from...";
+            this.loadFromToolStripMenuItem.ToolTipText = "CTRL + ALT + O";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(382, 344);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.label_std);
+            this.Controls.Add(this.label_average);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox_reactionTimes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBox_reaction);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_randomLetter);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Type Reaction App®";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromToolStripMenuItem;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
+
+        private System.Windows.Forms.Button btn_Start;
+
+        private System.Windows.Forms.Button btn_Exit;
+
+        private System.Windows.Forms.Label label_average;
+        private System.Windows.Forms.Label label_std;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.ListBox listBox_reactionTimes;
+
+        private System.Windows.Forms.TextBox txtBox_reaction;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label label_randomLetter;
 
         #endregion
     }
--- TypeReactionApp/Form1.cs
+++ TypeReactionApp/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,115 @@ namespace TypeReactionApp
 {
     public partial class Form1 : Form
     {
+        private Random random = new Random();
+        private Stopwatch stopwatch = new Stopwatch();
+        private Timer countdownTimer;
+        private int countdownValue = 3;
+        private bool isRunning = false;
+        
         public Form1()
         {
             InitializeComponent();
+            InitializeCountdownTimer();
+        }
+        
+        private void InitializeCountdownTimer()
+        {
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000; // 1 másodperc
+            countdownTimer.Tick += CountdownTimer_Tick;
+        }
+        
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (countdownValue > 0)
+            {
+                label_randomLetter.Text = countdownValue.ToString();
+                countdownValue--;
+            }
+            else
+            {
+                countdownTimer.Stop();
+                label_randomLetter.Text = ""; // Töröljük a visszaszámláló szöveget
+                GenerateRandomLetter();       // Véletlenszerű betű generálása
+                isRunning = true;
+                stopwatch.Start();
+                txtBox_reaction.Enabled = true;
+                txtBox_reaction.Focus();
+            }
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                // Indítás
+                countdownValue = 3;
+                label_randomLetter.Text = "3";
+                txtBox_reaction.Enabled = false;
+                listBox_reactionTimes.Items.Clear();
+                label_average.Text = "0 ms";
+                label_std.Text = "0 ms";
+                stopwatch.Reset();
+                btn_Start.Text = "Stop";
+                countdownTimer.Start();
+            }
+            else
+            {
+                // Leállítás
+                isRunning = false;
+                stopwatch.Stop();
+                btn_Start.Text = "Start";
+                label_randomLetter.Text = ""; // vagy alapértelmezett érték
+            }
+        }
+        
+        private void GenerateRandomLetter()
+        {
+            // Véletlenszerű betű kiválasztása A és Z között
+            char randomLetter = (char)random.Next('A', 'Z' + 1);
+            label_randomLetter.Text = randomLetter.ToString();
+        }
+
+        private void txtBox_reaction_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().ToUpper() == label_randomLetter.Text)
+            {
+                stopwatch.Stop();  // Megállítjuk az időzítőt
+                long reactionTime = stopwatch.ElapsedMilliseconds;  // Reakcióidő mérése
+
+                // Idő hozzáadása a listához
+                listBox_reactionTimes.Items.Add($"{reactionTime} ms");
+
+                UpdateStatistics();
+
+                // Új betű generálása és időzítő újraindítása
+                GenerateRandomLetter();
+                stopwatch.Restart();
+                txtBox_reaction.Clear();
+            }
+        }
+
+        
+        private void UpdateStatistics()
+        {
+            var times = listBox_reactionTimes.Items.Cast<string>()
+                .Select(item => long.Parse(item.Replace(" ms", "")))
+                .ToList();
+
+            if (times.Count > 0)
+            {
+                double average = times.Average();
+                double stdDev = Math.Sqrt(times.Average(v => Math.Pow(v - average, 2)));
+
+                label_average.Text = $@"{average:F0} ms";
+                label_std.Text = $@"{stdDev:F0} ms";
+            }
         }
     }
 }
